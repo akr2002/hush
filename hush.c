0397969625776d07ae2575f79dd4cef6c0fd56b4
Aditya <adityakr082@gmail.com>
2020-03-22 19:04:06 +0530

    added init and input functions

--- hush.c
+++ hush.c
@@ -10,3 +10,39 @@
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
 
+// Clearing the shell using escape sequences
+#define clear() printf("\033[H\033[J")
+
+// Greeting the shell during startup
+void init_shell()
+{
+	clear();
+	printf("***********\n
+		   hush	   \n
+		***********\n");
+	char *username = getenv("USER");
+	printf("\nUSER is: @%s", username);	
+	printf("\n");
+	sleep(1);
+	clear();
+}
+
+// FUnction to take input
+int takeInput(char* str)
+{
+	char *buf;
+
+	buf = readline("\n>>> ");
+	if (strlen(buf) != 0)
+	{
+		add_history(buf);
+		strcpy(str, buf);
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+
