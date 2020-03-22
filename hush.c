3d05d5ea93d19736b10d934e55c11e5e5ba263bc
Aditya <adityakr082@gmail.com>
2020-03-22 18:55:47 +0530

    inital commit

---
+++ hush.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <raedline/history.h>
+
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+
